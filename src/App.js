3ff2c0c784ad0c61ac253fc9e86e6584a17351aa
Anette <anet1@rocketmail.com>
2021-07-02 20:48:04 +0100

    fixed the functionality to open and close tabs

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ import './App.css';
 
 const options = [
   {
+    id: 1,
     question: 'How many team members can I invite? ',
     answer:
       'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
   },
   {
+    id: 2,
     question: 'What is the maximum file upload size?',
     answer: 'No more than 2GB. All files in your account must fit your allotted storage space.',
   },
   {
+    id: 3,
     question: 'How do I reset my password?',
     answer:
       'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
   },
   {
+    id: 4,
     question: 'Can I cancel my subscription?',
     answer: 'Yes! Send us a message and we’ll process your request no questions asked.',
   },
   {
+    id: 5,
     question: 'Do you provide additional support?',
     answer:
       'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
@@ -34,7 +39,19 @@ const App = () => {
   return (
     <div>
       <div className='main'>
-        <Card title='FAQ' options={options} />
+        <div className='card'>
+          <div className='cube'></div>
+          <div className='container'>
+            <div className='images'></div>
+
+            <div className='accordion'>
+              <h1 className='title'>FAQ</h1>
+              {options.map(option => (
+                <Card key={option.id} options={option} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,41 +4,26 @@ import arrow from '../../images/icon-arrow-down.svg';
 import '../../App.css';
 
 const Card = ({ options, title }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [visible, setVisible] = useState(false);
 
-  const onTitleClick = index => {
-    setActiveIndex(index);
+  const hiddenWhenVisible = { display: visible ? '' : 'none' };
+
+  const onTitleClick = () => {
+    console.log(visible);
+    setVisible(!visible);
   };
 
-  const renderedOptions = options.map((option, index) => {
-    return (
-      <div key={option.question}>
-        <div
-          onClick={() => onTitleClick(index)}
-          className={`flex ${index === activeIndex ? '' : 'active'}`}
-        >
-          <p className={`question ${index === activeIndex ? 'bold' : ''}`}>{option.question}</p>
-          <img className={`${index === activeIndex ? 'arrow' : ''}`} src={arrow} alt='arrow' />
-        </div>
-        <div className={`content ${index === activeIndex ? '' : 'active'}`}>
-          <p className='answer'>{option.answer}</p>
-        </div>
-        <div className='line'></div>
-      </div>
-    );
-  });
-
   return (
-    <div className='card'>
-      <div className='cube'></div>
-      <div className='container'>
-        <div className='images'></div>
-        <div className='accordion'>
-          <h1 className='title'>{title}</h1>
-          <div>{renderedOptions}</div>
-        </div>
+    <React.Fragment>
+      <div onClick={onTitleClick} className='flex hover'>
+        <p className={`question ${!options.id === visible ? '' : 'bold'}`}>{options.question}</p>
+        <img className={`${!options.id === visible ? '' : 'arrow'}`} src={arrow} alt='arrow' />
       </div>
-    </div>
+      <div style={hiddenWhenVisible}>
+        <p className='answer'>{options.answer}</p>
+      </div>
+      <div className='line'></div>
+    </React.Fragment>
   );
 };
 
